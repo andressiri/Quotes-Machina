6f69be1d0dd25df5cff95280f938117a61a14e52
Andrés Siri <andres.siri@hotmail.com>
2022-02-28 16:10:55 -0300

    RQM-4 - Create endpoint /quotes/:id

---
+++ Quotes.js
@@ -0,0 +1,55 @@
+const quotes = [
+    {
+        id: 1,
+        quote: "Quote 1",
+        author: "Author 1"
+    },
+    {
+        id: 2,
+        quote: "Quote 2",
+        author: "Author 2"
+    },
+    {
+        id: 3,
+        quote: "Quote 3",
+        author: "Author 3"
+    },
+    {
+        id: 4,
+        quote: "Quote 4",
+        author: "Author 4"
+    },
+    {
+        id: 5,
+        quote: "Quote 5",
+        author: "Author 5"
+    },
+    {
+        id: 6,
+        quote: "Quote 6",
+        author: "Author 6"
+    },
+    {
+        id: 7,
+        quote: "Quote 7",
+        author: "Author 7"
+    },
+    {
+        id: 8,
+        quote: "Quote 8",
+        author: "Author 8"
+    },
+    {
+        id: 9,
+        quote: "Quote 9",
+        author: "Author 9"
+    },
+    {
+        id: 10,
+        quote: "Quote 10",
+        author: "Author 10"
+    },
+
+];
+
+module.exports = quotes;
--- index.js
+++ index.js
@@ -1,60 +1,7 @@
 const express = require("express");
+const quotes = require("./Quotes");
 
 const app = express();
-
-const quotes = [
-    {
-        id: 1,
-        quote: "Quote 1",
-        author: "Author 1"
-    },
-    {
-        id: 2,
-        quote: "Quote 2",
-        author: "Author 2"
-    },
-    {
-        id: 3,
-        quote: "Quote 3",
-        author: "Author 3"
-    },
-    {
-        id: 4,
-        quote: "Quote 4",
-        author: "Author 4"
-    },
-    {
-        id: 5,
-        quote: "Quote 5",
-        author: "Author 5"
-    },
-    {
-        id: 6,
-        quote: "Quote 6",
-        author: "Author 6"
-    },
-    {
-        id: 7,
-        quote: "Quote 7",
-        author: "Author 7"
-    },
-    {
-        id: 8,
-        quote: "Quote 8",
-        author: "Author 8"
-    },
-    {
-        id: 9,
-        quote: "Quote 9",
-        author: "Author 9"
-    },
-    {
-        id: 10,
-        quote: "Quote 10",
-        author: "Author 10"
-    },
-
-];
 
 app.get('/', (req, res) => {
     res.send('<h1>Insert Quote here<h1>');
@@ -63,6 +10,15 @@ app.get('/', (req, res) => {
 app.get('/quotes', (req, res) => {
     const randomQuote = quotes[Math.floor(Math.random() * 10)];
     res.json(randomQuote);
+});
+
+app.get('/quotes/:id', (req, res) => {
+    const found = quotes.some(quote => quote.id === parseInt(req.params.id));
+    if (found) {
+        res.json(quotes.filter(quote => quote.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({msg: `No quote with the id of ${req.params.id}` })
+    };    
 });
 
 const PORT = process.env.PORT || 5000;
